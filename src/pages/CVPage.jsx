3afe540b1bbdf8044b8f164e2f87532ca98a223f
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-10-15 06:02:11 +0300

    Finish CV Page

--- src/pages/CVPage.jsx
+++ src/pages/CVPage.jsx
@@ -1,15 +1,23 @@
-import React, { Fragment } from 'react'
-import { PDFViewer } from '@react-pdf/renderer'
-import { Header, Footer, CV } from '@/components'
+import React, { Fragment } from 'react';
+// Component
+import { Header, Footer } from '@/components';
+// Document
+import CVDoc from '@/assets/documents/cv.pdf';
 
 export const CVPage = () => {
     return (
         <Fragment>
             <Header />
-            <PDFViewer className='w-full h-screen'>
-                <CV />
-            </PDFViewer>
+            <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
+                <h1 className="text-3xl font-bold m-8">My Curriculum Vitae</h1>
+                <iframe
+                    src={CVDoc}
+                    width="100%"
+                    className="flex-grow border-2 border-gray-300 shadow-lg"
+                    title="Mohamed CV"
+                />
+            </div>
             <Footer />
         </Fragment>
-    )
-}
+    );
+};
